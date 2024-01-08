724140e39a7888adaace572e77a663c7c1aee60d
JuliaLotnikova <150443868+JuliaLotnikova@users.noreply.github.com>
2024-01-08 15:30:26 +0300

    v2

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let a, b, result;
 
+if (a > 10) {
+  console.log("a больше 10");
+} else if (a > 5) {
+  console.log("a больше 5, но не больше 10");
+} else if (a > 2) {
+  console.log("a больше 2, но не больше 5");
+} else {
+  console.log("a меньше или равно 2");
+}
+
 switch (true) {
   case (a > 10):
     console.log("a больше 10");
